be974f1336b23da3e4229e8a6547bb5e94195dbb
rbrauner <rafalbrauner@gmail.com>
2017-09-21 16:04:56 +0200

    Fix problems with white chars

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -7,10 +7,11 @@ const int BUILT_IN_LED = 13;
 int main() {
   init();
   Serial.begin(9600);
+	
+  pinMode(BUILT_IN_LED, OUTPUT);
+  pinMode(LED, OUTPUT);
 
-	pinMode(BUILT_IN_LED, OUTPUT);
+  while (true) {
 
-	while (true) {
-
-	}
+  }
 }
