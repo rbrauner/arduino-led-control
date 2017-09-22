be5bcc4a119d60fe8b879ed218744af40749c029
rbrauner <rafalbrauner@gmail.com>
2017-09-22 15:24:35 +0200

    Define SerialInfo

--- gui/src/serialinfo.hpp
+++ gui/src/serialinfo.hpp
@@ -1,6 +1,19 @@
 #ifndef SERIALINFO_HPP
 #define SERIALINFO_HPP
 
-struct SerialInfo {};
+#include <QSerialPort>
+#include <QString>
+
+struct SerialInfo {
+  bool isAviable;
+  QString portName;
+  const quint16 vendorId;
+  const quint16 productId;
+  QSerialPort::BaudRate baudRate;
+  QSerialPort::DataBits dataBits;
+  QSerialPort::Parity parity;
+  QSerialPort::StopBits stopBits;
+  QSerialPort::FlowControl flowControl;
+};
 
 #endif // SERIALINFO_HPP
